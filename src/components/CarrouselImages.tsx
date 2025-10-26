c0efc9456caa632c3ec9e1c0d3e691a855f15ae1
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-26 17:17:57 -0300

    feat: Carrousel Images component

---
+++ src/components/CarrouselImages.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+
+export default function CarrouselImages() {
+  return (
+    <div className="flex gap-10">
+      <Image src={} alt="" width={} height={} />
+    </div>
+  );
+}
