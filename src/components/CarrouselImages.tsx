d24f0b02d8f35ad885fe2d773cf9c0bf0fc968ac
Gabriel Bueno Hygino <gabriel.buenohyginoc@gmail.com>
2025-10-26 17:39:54 -0300

    feat: shadcnui

--- src/components/CarrouselImages.tsx
+++ src/components/CarrouselImages.tsx
@@ -2,35 +2,37 @@ import Image from 'next/image';
 import ImageHome from '../../public/images/home.jpg';
 export default function CarrouselImages() {
   return (
-    <div className="flex gap-10 py-16">
-      <Image
-        src={ImageHome}
-        alt=""
-        width={250}
-        height={250}
-        className="rounded-2xl"
-      />
-      <Image
-        src={ImageHome}
-        alt=""
-        width={250}
-        height={250}
-        className="rounded-2xl"
-      />
-      <Image
-        src={ImageHome}
-        alt=""
-        width={250}
-        height={250}
-        className="rounded-2xl"
-      />
-      <Image
-        src={ImageHome}
-        alt=""
-        width={250}
-        height={250}
-        className="rounded-2xl"
-      />
+    <div className="py-16">
+      <div className="flex gap-10">
+        <Image
+          src={ImageHome}
+          alt=""
+          width={250}
+          height={250}
+          className="rounded-2xl"
+        />
+        <Image
+          src={ImageHome}
+          alt=""
+          width={250}
+          height={250}
+          className="rounded-2xl"
+        />
+        <Image
+          src={ImageHome}
+          alt=""
+          width={250}
+          height={250}
+          className="rounded-2xl"
+        />
+        <Image
+          src={ImageHome}
+          alt=""
+          width={250}
+          height={250}
+          className="rounded-2xl"
+        />
+      </div>
     </div>
   );
 }
